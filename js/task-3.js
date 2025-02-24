722149cc1f79312e5a4c97a79e08ef9d78fec808
Maksym <makseemer@gmail.com>
2025-02-24 20:39:25 +0200

    something

--- js/task-3.js
+++
@@ -1,9 +0,0 @@
-
-const nameInput = document.getElementById('name-input');  
-const nameOutput = document.getElementById('name-output');  
-
-nameInput.addEventListener('input', () => {  
-    const name = nameInput.value.trim();  
-    
-    nameOutput.textContent = name === '' ? 'Anonymous' : name;  
-});
